1cab46759a78b6b8ca44e8c3e8d3424f79ccbc92
cmarvann <covingtonmarvann@gmail.com>
2022-07-11 22:50:46 -0400

    create and execute an application wit hnode.js

--- Readme.js
+++ Readme.js
@@ -1,2 +1,23 @@
-const inquirer = require['inquirer'];
-console.log('Ready set go')
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+// arrow function ES6
+const printProfileData = profileDataArr => {
+  profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+// The print call for dynamic output 
+printProfileData(profileDataArgs);
+
+
+// console.log(generatePage('Marvann', 'cmarvannhub'));
+
+// // Multi-line Strings
+// const generatePage = (userName, githubName) => {
+//     return `
+//       Name: ${userName}
+//       GitHub: ${githubName}
+//     `;
+//   };
+//   console.log(Marvan, cmarvannhub);
+
