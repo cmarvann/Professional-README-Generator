30aa4fa66b5a4d12cf65d822b7ea6c1ecfa72040
cmarvann <covingtonmarvann@gmail.com>
2022-07-14 19:24:10 -0400

    add updates to js file

--- Readme.js
+++ Readme.js
@@ -1,19 +1,118 @@
-// add fs statement
+const inquirer = require('inquirer');
+
 const fs = require('fs');
-const generatePage = require('./src/page-template');
 
-// function that recieve the command line argument and 
-// insert in an HTML template literl
+const generateMarkdown = require('./utils/generateMarkdown');
 
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
-console.log(name, github);
+// array
+let questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name? (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your name!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username (Required)',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Enter your GitHub username!');
+                return false;
+            }
+        }
+    },
 
-// The String 
+    {
+        type: 'input',
+        name: 'link',
+        message: 'Enter the GitHub link to your project. (Required)',
+        validate: linkInput => {
+            if (linkInput) {
+                return true;
+            } else {
+                console.log('Enter your project GitHub link!');
+                return false;
+            }
+        }
+    },
 
-// End of argument
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
-    console.log('See output, index.html!');
-  });
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project? (Required)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Enter the title of your  project!');
+                return false;
+            }
+        }
+
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of your project (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Enter your project description!');
+                return false;
+            }
+        }
+    },
+    {
+    type: 'input',
+    name: 'license',
+    message: 'Enter your project License (Required)',
+    validate: licenseInput => {
+        if (licenseInput) {
+            return true;
+        } else {
+            console.log('Enter name of license used for your project!');
+            return false;
+        }
+    }
+},
+
+];
+
+// Declare function
+function startQuestions(){
+
+    // prompt
+    inquirer.prompt(questions)
+    // grab answers
+    // .then(function(answers){
+    //  console.log(answers);
+    //  console.log(answers.title)
+    // })
+    .then(answers => {
+        fs.writeFile("example.md", generateMarkdown(answers), err => {
+            if (err) {
+                console.log(err);
+                return;
+              }
+              console.log('Style sheet copied successfully!');
+          
+        })
+    // }
+        // generateMarkdown(answers)
+    })
+}
+// call function - run function
+startQuestions()
 
